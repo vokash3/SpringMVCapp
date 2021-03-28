bf1ea10844ecbe262c8e9dbc74ffad69c8ffd928
wolldemurr <vokash3@yandex.ru>
2021-03-28 15:12:31 +0300

    * Добавлена БД PostgreSQL * Обновлены методы класса PersonDAO.java для взаимодействия с БД Connection -> Statement (PreparedStatement) -> ResultSet

--- src/main/java/ru/wain/springmvcapp/DAO/PersonDAO.java
+++ src/main/java/ru/wain/springmvcapp/DAO/PersonDAO.java
@@ -3,42 +3,127 @@ package ru.wain.springmvcapp.DAO;
 import org.springframework.stereotype.Component;
 import ru.wain.springmvcapp.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component // Spring создаст бин этого класса и внедрит его в PeopleController
 public class PersonDAO {
 
-    private List<Person> people; // Временная замена БД (включая нестатический блок)
-    private static int PERSON_INDEX;
-    {
-        people = new ArrayList<Person>();
-        people.add(new Person(++PERSON_INDEX, "Tom", 12, "tom@cartoon.org"));
-        people.add(new Person(++PERSON_INDEX, "Jerry", 11, "jerry@cartoon.org"));
-        people.add(new Person(++PERSON_INDEX, "Spike", 14, "spike@cartoon.org"));
+    private static String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static String USERNAME = "postgres";
+    private static String PASSWORD = "postgres";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver"); //Подгрузка драйвера через рефлексию
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Person> index(){
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        String SQL = "SELECT * FROM Person WHERE id=?";
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return person;
     }
 
     public void save(Person person) {
-        person.setId(++PERSON_INDEX); //Так как id как бы динамически инкриментируется при добавлении в БД
-        people.add(person);
+//        person.setId(++PERSON_INDEX); //Так как id как бы динамически инкриментируется при добавлении в БД
+//        people.add(person);
+        try {
+            String SQL = "INSERT INTO Person VALUES (1, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void update(int id, Person person) {
-        Person personToUpdate = show(id);
-        personToUpdate.setName(person.getName());
-        personToUpdate.setAge(person.getAge());
-        personToUpdate.setEmail(person.getEmail());
+//        Person personToUpdate = show(id);
+//        personToUpdate.setName(person.getName());
+//        personToUpdate.setAge(person.getAge());
+//        personToUpdate.setEmail(person.getEmail());
+        String SQL = "UPDATE Person SET name=?, age=?, email=? WHERE id=?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+//        people.removeIf(p -> p.getId() == id);
+        String SQL = "DELETE FROM Person WHERE id=?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
